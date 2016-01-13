7749679c3096a41c82e083fd8aa4e49adb712397
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-01-13 02:52:24 +0000

    Working on server stuff.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+
+var serv_url = 'http://forty7.guru/ira/serv'
+
+
+
 var app = angular.module('iraBlog', []);
-app.controller('iraBlog', function($scope) {
-   $scope.countries = [
+app.controller('iraBlog', function($scope, $http) {
+   $scope.countries = [];
+   
+   $scope.getData = function (dataToGet, callBack) {
+      var req_url =  serv_url + '?data=' + dataToGet;
+      $http.get(req_url).then(function (data) {
+         callBack(data.data);
+      });
+   };
+
+   function setCountries(countries) {
+      $scope.countries = countries;
+   }
+   $scope.getData('countryCount', setCountries);
+
+   /*[
       {'name': 'US', 'count': 1},
       {'name': 'Russia', 'count': 5}
+   ];*/
 
-   ];
    $scope.countrySelect = function(country) {
       alert(country.name);
    }
--- server/server.js
+++ server/server.js
@@ -9,32 +9,45 @@ app.use(bodyParser.json());
 /*app.use(bodyParser.urlencoded({
    extended: true
 }));*/
-
-app.get('/assmilk', function(req, res) {
-   res.set('Access-Control-Allow-Origin', '*');
-
-   console.log(req);
-
-   response = [
-      {'first':'Helen', 'last':'Cabanillas', 'country':'Peru'},
-      {'first':'Kostyantyn', 'last':'Kovalskyy', 'country':'Ukraine'},
-      {'first':'Tony', 'last':'Klimov', 'country':'USA'},
-      {'first':'Ira', 'last':'Klimov', 'country':'Ukraine'}
-
-   ];
-
-   res.send(200, response);
-});
-
 function onErr(err) {
    if (err) console.log(err);
 }
 
-app.post('/assmilk', function(req, res) {
+app.get('/ira/serv', function(req, res) {
+   res.set('Access-Control-Allow-Origin', '*');
+   //console.log(req);
+   //console.log(req.body);
+
+   var query = req.query;
+   if (query.data == 'countryCount')
+      console.log('getting country count');
+
+   var response = [
+      {'name': 'US', 'count': 1},
+      {'name': 'Russia', 'count': 5}
+   ];
+   /*response = [
+      {'first':'Helen', 'last':'Cabanillas', 'country':'Peru'},
+      {'first':'Kostyantyn', 'last':'Kovalskyy', 'country':'Ukraine'},
+      {'first':'Tony', 'last':'Klimov', 'country':'USA'},
+      {'first':'Ira', 'last':'Klimov', 'country':'Ukraine'}
+   ];*/
+   //var data = fs.readFileSync('../data/post1449271171');
+   /*var data = fs.readdirSync('../data/');
+   response = data + '';*/
+
+    
+
+   //res.send(200, response);
+   res.status(200).send(response);
+});
+
+
+app.post('/ira/serv', function(req, res) {
    console.log(req.body);
    fs.writeFile('/tmp/test', req.body, onErr);
 });
 
-app.listen(6969);
+app.listen(1342);
 
 
