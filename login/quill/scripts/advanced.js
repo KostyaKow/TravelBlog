60c2a38a60deae9d4016f2192c4a254ac446f826
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-01-21 22:23:58 +0000

    Modifying quilt example for our purposes.

--- login/quill/scripts/advanced.js
+++ login/quill/scripts/advanced.js
@@ -2,18 +2,6 @@ var _, advancedEditor, authorship, basicEditor, cursorManager;
 
 _ = Quill.require('lodash');
 
-/*basicEditor = new Quill('.basic-wrapper .editor-container', {
-  modules: {
-    authorship: {
-      authorId: 'basic'
-    },
-    toolbar: {
-      container: '.basic-wrapper .toolbar-container'
-    }
-  },
-  styles: false
-});*/
-
 advancedEditor = new Quill('.advanced-wrapper .editor-container', {
   modules: {
     'authorship': {
@@ -37,26 +25,8 @@ authorship.addAuthor('basic', 'rgba(255,153,51,0.4)');
 
 cursorManager = advancedEditor.getModule('multi-cursor');
 
-cursorManager.setCursor('basic', 0, 'basic', 'rgba(255,153,51,0.9)');
+//cursorManager.setCursor('basic', 0, 'basic', 'rgba(255,153,51,0.9)');
 
-/*basicEditor.on('selection-change', function(range) {
-  console.info('basic', 'selection', range);
-  if (range != null) {
-    return cursorManager.moveCursor('basic', range.end);
-  }
-});*/
-
-/*basicEditor.on('text-change', function(delta, source) {
-  var sourceDelta, targetDelta;
-  if (source === 'api') {
-    return;
-  }
-  console.info('basic', 'text', delta, source);
-  advancedEditor.updateContents(delta);
-  sourceDelta = basicEditor.getContents();
-  targetDelta = advancedEditor.getContents();
-  return console.assert(_.isEqual(sourceDelta, targetDelta), "Editor diversion!", sourceDelta.ops, targetDelta.ops);
-});*/
 
 advancedEditor.on('selection-change', function(range) {
   return console.info('advanced', 'selection', range);
