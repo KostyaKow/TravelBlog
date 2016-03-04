4feaad2dd09de0f59221a6ae00053cb2477a886b
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-03-04 02:27:26 +0000

    Working on Admin side-panel.

--- login/index.js
+++ login/index.js
@@ -1,16 +1,33 @@
 
-var serv_url = 'http://forty7.guru/ira/'
-function jGet(url, callback) {
-   $.get(url, callback);
-}
-
-function getData(dataToGet, callback) {
-   var req_str = serv_url + 'serv?';
-}
-
 
 $(document).ready(() => {
-   $('#sidepanel li').addClass('list-group-item');
+   var adminOpts = $('#sidepanel li');
+   adminOpts.addClass('list-group-item');
+   //adminOpts.css('cursor', 'pointer');
+
+   adminOpts.click(function(e) {
+      loadPage($(this).text());
+   });
+
+   loadPage('Pages'); //default
 });
 
+function loadPage(name) {
+   var adminOpts = $('#sidepanel li');
+   adminOpts.removeClass('selected');
+   adminOpts.each(function() {
+      var currName = $(this).text();
+      if (name == currName)
+         $(this).addClass('selected');
+   });
+}
 
+function setEntriesList() {
+   var entriesList = $('#entriesList');
+
+   getData('getBlogPosts', (postList) => {
+      for (var post in postList)
+         alert(post);
+
+   });
+}
